6acf468222c8ea632f5278b1693207762be731cb
gitzman <adam.i.gitzes@gmail.com>
2015-11-01 22:29:08 -0500

    fixed

--- flasky.py
+++ flasky.py
@@ -15,3 +15,7 @@ def index():
 @app.route("/3d/<start>")
 def thirdd(start):
     return app.send_static_file(makenotebook.editNotebook(start))
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
